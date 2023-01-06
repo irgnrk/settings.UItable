31f029501d8c575f38d6be6c46ddc5336a37ab4e
ivan <houndby@gmail.com>
2023-01-06 14:09:43 +0300

    Initial Commit

---
+++ settings.UItable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  settings.UItable
+//
+//  Created by Иван Быховский on 6.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
