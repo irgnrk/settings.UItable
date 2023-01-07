42dc67b7cc922287a45e7266199b6cea5a9796db
IvanBykhauski <houndby@gmail.com>
2023-01-07 19:53:39 +0300

    2 commit

--- settings.UItable/Sourses/Model.swift
+++ settings.UItable/Sourses/Model.swift
@@ -24,10 +24,16 @@ class Model: ModelProtocol {
         [ModelItems(icon: "avatar", firstName: "Ivan", secondName: "iCloud", switchFlag: false)],
         //2
         [ModelItems(icon: "aviamode", firstName: "Авиарежим", secondName: "", switchFlag: true),
-        ModelItems(icon: "wifi", firstName: "Wi-Fi", secondName: "name", switchFlag: false)],
+        ModelItems(icon: "wifi", firstName: "Wi-Fi", secondName: "name", switchFlag: false),
+        ModelItems(icon: "bluetooth", firstName: "bluetooth", secondName: "Вкл", switchFlag: false),
+        ModelItems(icon: "netconnect", firstName: "Сотовая связь", secondName: "", switchFlag: false),
+        ModelItems(icon: "modemmode", firstName: "Режим модема", secondName: "", switchFlag: false),
+        ModelItems(icon: "vpn", firstName: "VPN", secondName: "", switchFlag: true)],
         //3
         [ModelItems(icon: "notifications", firstName: "Уведомления", secondName: "", switchFlag: false),
-         ModelItems(icon: "sound", firstName: "Звуки и тактильные сигналы", secondName: "", switchFlag: false)
+         ModelItems(icon: "sound", firstName: "Звуки и тактильные сигналы", secondName: "", switchFlag: false),
+         ModelItems(icon: "focus", firstName: "Фокусирование", secondName: "", switchFlag: false),
+         ModelItems(icon: "time", firstName: "Экранное время", secondName: "", switchFlag: false)
         ]
     ]
 }
--- settings.UItable/Sourses/ViewController.swift
+++ settings.UItable/Sourses/ViewController.swift
@@ -9,13 +9,18 @@ class ViewController: UIViewController {
     
     //MARK: - UI elements
     
+    private lazy var tableFontOne = UIFont.systemFont(ofSize: 12)
+    private lazy var tableFontTwo = UIFont.systemFont(ofSize: 10)
+    
     private lazy var myTableView: UITableView = {
         let tableview = UITableView(frame: CGRect.zero, style: .insetGrouped)
         tableview.backgroundColor = .yellow
         tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell2")
         tableview.dataSource = self
         tableview.delegate = self
         tableview.translatesAutoresizingMaskIntoConstraints = false
+        tableview.rowHeight = 50
         return tableview
         
     } ()
@@ -33,8 +38,8 @@ class ViewController: UIViewController {
         view.backgroundColor = .orange
         title = "Settings:"
         let bar = navigationController?.navigationBar
-        //bar?.prefersLargeTitles = true // большой текст
-        bar?.layer.backgroundColor = CGColor(red: 254/255, green: 17/255, blue: 242/255, alpha: 1)
+        bar?.prefersLargeTitles = true // большой текст
+       // bar?.layer.backgroundColor = CGColor(red: 254/255, green: 17/255, blue: 242/255, alpha: 1)
         bar?.layer.cornerRadius = 10
     }
     
@@ -71,12 +76,44 @@ extension ViewController: UITableViewDataSource {
     
     //настройка ячейки
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-            let cellData = model.models[indexPath.section][indexPath.row]
-            let myImage = UIImage(named: cellData.icon)
-            cell.textLabel?.text = cellData.firstName
-        cell.imageView?.image =  myImage
-        return cell
+            if indexPath.section == 0 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+                let cellData = model.models[indexPath.section][indexPath.row]
+                let myImage = UIImage(named: cellData.icon)
+              
+                var content = cell.defaultContentConfiguration()
+                content.image = myImage
+                content.text = cellData.firstName
+                content.imageProperties.cornerRadius = tableView.rowHeight / 4
+                content.imageProperties.maximumSize = CGSize(width: tableView.rowHeight, height: tableView.rowHeight)
+                cell.contentConfiguration = content
+                return cell
+            } else {
+               
+                let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+                let cellData = model.models[indexPath.section][indexPath.row]
+                let myImage = UIImage(named: cellData.icon)
+               
+                var content = cell.defaultContentConfiguration()
+                content.image = myImage
+                content.text = cellData.firstName
+                content.textProperties.font = tableFontOne
+                content.imageProperties.cornerRadius = tableView.rowHeight / 4
+                content.imageProperties.maximumSize = CGSize(width: tableView.rowHeight - 5, height: tableView.rowHeight - 5 )
+                content.secondaryText = cellData.secondName
+                content.secondaryTextProperties.font = tableFontTwo
+                content.prefersSideBySideTextAndSecondaryText = true
+                cell.contentConfiguration = content
+                
+                if !cellData.switchFlag {
+                    cell.accessoryView = UIImageView(image: UIImage(systemName: "arrowtriangle.forward"))
+                } else {
+                    cell.accessoryView = UISwitch()
+                }
+                
+           //     cell.backgroundColor = .systemPink
+                return cell
+            }
     }
     
 }
@@ -85,6 +122,7 @@ extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     
    
